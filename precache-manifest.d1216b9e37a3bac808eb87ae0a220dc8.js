c3a443fd699aa3b1420b4363bab7192b6c01d609
Kaito omura <omura.kaito@realsys.co.jp>
2018-11-14 16:26:44 +0900

    Updates

---
+++ precache-manifest.d1216b9e37a3bac808eb87ae0a220dc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8566fc086f75c595e3142be74f6812d",
+    "url": "/react-photo-gallery/static/media/bg-home.d8566fc0.jpg"
+  },
+  {
+    "revision": "40d8e2a528e47d10bf0c",
+    "url": "/react-photo-gallery/static/js/runtime~main.40d8e2a5.js"
+  },
+  {
+    "revision": "0427c2be4cae12cc4c20",
+    "url": "/react-photo-gallery/static/js/main.0427c2be.chunk.js"
+  },
+  {
+    "revision": "72c71befa9186682b8ad",
+    "url": "/react-photo-gallery/static/js/1.72c71bef.chunk.js"
+  },
+  {
+    "revision": "0427c2be4cae12cc4c20",
+    "url": "/react-photo-gallery/static/css/main.440cdaa3.chunk.css"
+  },
+  {
+    "revision": "72c71befa9186682b8ad",
+    "url": "/react-photo-gallery/static/css/1.cd2ba1a3.chunk.css"
+  },
+  {
+    "revision": "60f42a25a4f27d1401feccd8994e3df7",
+    "url": "/react-photo-gallery/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec765aae52da54a0b90afcd6c8b8c0df.js"
+  "/react-photo-gallery/precache-manifest.d1216b9e37a3bac808eb87ae0a220dc8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-photo-gallery/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
